58c703342ddcba2cafe837454391e31eef7aab01
Sachin Gaikwad <sachin.gaikwad@safesend.com>
2023-02-10 19:50:58 +0530

    state, eventhandler, list

---
+++ animals/src/App.js
@@ -0,0 +1,34 @@
+import AnimalShow from "./AnimalShow";
+import { useState } from "react";
+import "./App.css"
+
+function getRandomAnimal() {
+  const animals = ["cow", "cat", "bird", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
+  return (
+    <>
+      <div className="app">
+        <button onClick={handleClick}>Add Animal</button>
+        <div className="animal-list">
+          {renderedAnimals}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
