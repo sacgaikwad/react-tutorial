6b02712f98471dee862335e8efbdae908a55907e
Sachin Gaikwad <sachin.gaikwad@safesend.com>
2023-02-20 19:18:13 +0530

    more on react

---
+++ components-sample/src/App.js
@@ -0,0 +1,26 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      app
+      <div>
+        <Button success rounded outline>Click!</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Hide Ads</Button>
+      </div>
+      <div>
+        <Button>Submit</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ components-sample/src/index.js
@@ -0,0 +1,9 @@
+import react from 'react'
+import  ReactDOM  from 'react-dom/client'
+import App from './App'
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(<App></App>)
+
