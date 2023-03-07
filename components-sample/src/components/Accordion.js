d45a1765867df1dec57636891e0e812ff412f760
Sachin Gaikwad <sachin.gaikwad@safesend.com>
2023-03-07 14:48:24 +0530

    hybrid eventhandler shorthand + longhand

--- components-sample/src/components/Accordion.js
+++ components-sample/src/components/Accordion.js
@@ -3,28 +3,25 @@ import { useState } from "react";
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
-  
+  const handleClick = (index) => {
+    setExpandedIndex(index);
+  };
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    const handleClick = () => {
-      setExpandedIndex(index);
-    };
-
-
-    return (
-      <div key={item.id}>
-        <div onClick={handleClick}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
-      </div>
-    );
     // return (
     //   <div key={item.id}>
-    //     <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+    //     <div onClick={handleClick}>{item.label}</div>
     //     {isExpanded && <div>{item.content}</div>}
     //   </div>
     // );
+    return (
+      <div key={item.id}>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
   });
 
   return <div>{renderedItems}</div>;
